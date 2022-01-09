e1ff3f3dea81c9d9541e681492d8fb9be76f5d3f
Wilfred <wilfredbayudan@gmail.com>
2022-01-09 01:25:41 -1000

    commit tree d3bb6c70cdfe8733a286692dc220e1c73520c18c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+  
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_validate
+
+  def clickbait_validate
+
+    if title
+      clickbait_words = [
+        "Won't Believe",
+        "Secret",
+        "Top",
+        "Guess" 
+      ]
+
+      unless clickbait_words.any? { |heading| title.include?(heading)}
+        errors.add(:title, "Title must contain clickbait.")
+      end
+
+    end
+
+  end
+
 end
